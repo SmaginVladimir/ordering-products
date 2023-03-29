39a0c0b9209f743da3563cb9a7bede25c91e6dc5
Smagin Vladimir <smagin.vovan09@gmail.com>
2023-03-29 21:13:23 +0600

    added resources Category Product All

---
+++ app/Http/Resources/CategoryProductAllResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class CategoryProductAllResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|JsonSerializable|Arrayable
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'image' => $this->image,
+            'products' => ProductResource::collection($this->products)
+        ];
+    }
+}
