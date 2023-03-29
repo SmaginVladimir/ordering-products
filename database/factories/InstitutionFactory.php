7d931efe9db6e8a65a2bcfe6d44c5aac4713384e
Smagin Vladimir <smagin.vovan09@gmail.com>
2023-03-29 21:07:55 +0600

    added factory Institutions

---
+++ database/factories/InstitutionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Institution>
+ */
+class InstitutionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'image' => $this->faker->unique()->imageUrl,
+            'description' => $this->faker->unique()->realTextBetween,
+            'address' => $this->faker->streetAddress
+        ];
+    }
+}
